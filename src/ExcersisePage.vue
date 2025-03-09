52b7f626bae2b3ddc6b46f13fdf1c6ace6bcaf24
Kasper Wolsink <kwolsink@gmail.com>
2025-03-09 16:00:38 +0100

    Divide into pages

---
+++ src/ExcersisePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <Title>Bench</Title>
+    <div class="chart-container">
+      <LogChart class="component chart" :log-entries="logEntries"></LogChart>
+    </div>
+    <LogList class="component" :log-entries="logEntries"></LogList>
+    <SubmissionForm class="component" @submit="(entry) => logEntries.push(entry)"></SubmissionForm>
+  </div>
+</template>
+
+
+<script setup>
+import LogList from "./components/LogList.vue";
+import SubmissionForm from "./components/SubmissionForm.vue";
+import {ref} from "vue";
+import Title from "./components/Title.vue";
+import LogChart from "./components/LogChart.vue";
+
+const logEntries = ref([])
+
+
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.component {
+  margin-bottom: 2em;
+}
+
+.chart-container {
+  display: flex;
+  justify-content: center;
+}
+
+.chart {
+  max-width: 900px;
+  max-height: 500px;
+}
+
+
+</style>
---
+++ src/HomePage.vue
@@ -0,0 +1,56 @@
+<script setup>
+import {Button} from "primevue";
+
+function route() {
+
+}
+
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="quadrant top-left">
+      <Button class="exercise"  severity=“info” variant="outlined"><h2>Bench</h2></Button>
+    </div>
+    <div class="quadrant top-right">
+      <Button class="exercise" severity=“warn” variant="outlined"><h2>Squat</h2></Button>
+    </div>
+    <div class="quadrant bottom-left">
+      <Button class="exercise" severity=“danger” variant="outlined"><h2>Overhead press</h2></Button>
+    </div>
+    <div class="quadrant bottom-right">
+      <Button class="exercise" severity=“help” variant="outlined"><h2>Deadlift</h2></Button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+
+.quadrant {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.top-left { grid-area: 1 / 1; }
+.top-right { grid-area: 1 / 2; }
+.bottom-left { grid-area: 2 / 1; }
+.bottom-right { grid-area: 2 / 2; }
+
+.exercise {
+  width: 300px;
+  height: 150px;
+  font-style: italic;
+  margin: 1em;
+}
+
+
+</style>
