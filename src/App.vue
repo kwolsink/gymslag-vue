8518268a3ed7e635be7a984d50ba515583f2e6c8
Kasper Wolsink <kwolsink@gmail.com>
2025-03-09 12:57:19 +0100

    Add Log Table

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <LogList></LogList>
 </template>
 
 
 <script setup>
 import Button from "primevue/button"
+import LogList from "./components/LogList.vue";
 
 
 </script>
---
+++ src/components/LogList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {DataTable, Column} from "primevue"
+
+const testData = [
+  {
+    name: "Kasper",
+    reps: 5,
+    weightKg: 100,
+    projectedMax: 120,
+    date: Date.now()
+  }
+]
+
+</script>
+
+<template>
+  <DataTable :value="testData">
+    <Column field="name" header="Naam"></Column>
+    <Column field="reps" header="Reps"></Column>
+    <Column field="weightKg" header="Gewicht"></Column>
+    <Column field="projectedMax" header="Projected 1RM"></Column>
+    <Column field="date" header="Datum"></Column>
+  </DataTable>
+</template>
+
+<style scoped>
+
+</style>
