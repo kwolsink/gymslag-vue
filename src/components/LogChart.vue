90b16bd0684c537883192cb5a2703399bfda3a00
Kasper Wolsink <kwolsink@gmail.com>
2025-03-09 21:36:15 +0100

    Remove log statement

--- src/components/LogChart.vue
+++ src/components/LogChart.vue
@@ -16,7 +16,6 @@ watch(() => props.logEntries, async (logEntries) => {
 
 function toChartData(logEntries) {
   logEntries.sort((a, b) => a.date - b.date)
-  console.log(logEntries)
   const entriesByName = logEntries.reduce((map, entry) => {
     if (!map.has(entry.lifter)) {
       map.set(entry.lifter, []);
